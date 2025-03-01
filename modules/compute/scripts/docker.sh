a17ab955d50b46be555663c092f8733526c22ba2
NataSoltero <josue.soltero@gmail.com>
2025-03-01 09:29:22 -0600

    Adding new inbound rule fore jenkins and updating the docker script

--- modules/compute/scripts/docker.sh
+++ modules/compute/scripts/docker.sh
@@ -12,6 +12,28 @@ sudo yum install -y docker
 echo "Starting Docker"
 sudo service docker start
 
+# Enabling the Docker service to start on boot
+echo "Enabling Docker to start on boot"
+sudo systemctl enable docker.service
+
 # Adding the ec2-user to the docker group
 echo "Adding ec2-user to the Docker group"
 sudo usermod -a -G docker ec2-user
+
+# Pulling the docker image
+echo "Pulling the Docker image"
+docker pull centos
+
+# Running the docker image with the sleep command to keep the container running
+echo "Running the Docker image"
+docker run -d centos sleep 180
+
+# Creating local directory to store the Jenkins data
+echo "Creating Jenkins data directory"
+mkdir /var/my-jenkins-data
+
+# Running docker jenkins image and mapping container port 8080 to host port 8080
+# Mapping jenkins_home to /var/my-jenkins-data
+# The credentilas can be found in /var/jenkins_home/secrets/initialAdminPassword
+echo "Running Jenkins Docker image"
+docker run -p 8080:8080 -v /var/my-jenkins-data:/var/jenkins_home -u root jenkins/jenkins
